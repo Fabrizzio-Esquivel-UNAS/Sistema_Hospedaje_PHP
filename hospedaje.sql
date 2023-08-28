b44185696f1114c868dce56b07c1727822f801b8
Fabrizzio Fabiano Esquivel Mori <fabrizzio.esquivel@unas.edu.pe>
2023-08-28 14:37:33 -0500

    Añadido de base de datos

---
+++ hospedaje.sql
@@ -0,0 +1,263 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 27, 2023 at 09:46 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hospedaje`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alquileres`
+--
+
+CREATE TABLE `alquileres` (
+  `id` int NOT NULL,
+  `id_huesped` int DEFAULT NULL,
+  `id_habitacion` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `id_recepcionista` int DEFAULT NULL,
+  `id_pago` int DEFAULT NULL,
+  `fecha_alquiler` date DEFAULT NULL,
+  `dias` int DEFAULT NULL,
+  `costo` double DEFAULT NULL,
+  `personas` int DEFAULT NULL,
+  `motivo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `procedencia` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `comentarios` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `habitaciones`
+--
+
+CREATE TABLE `habitaciones` (
+  `id` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id_alquiler` int DEFAULT NULL,
+  `tipo` enum('SIMPLE','DOBLE','FAMILIAR','MATRIMONIAL') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `precio` double DEFAULT NULL,
+  `desc` varchar(100) DEFAULT NULL,
+  `estado_limpieza` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `huespedes`
+--
+
+CREATE TABLE `huespedes` (
+  `id` int NOT NULL,
+  `nombres` varchar(100) DEFAULT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `doc_tipo` enum('DNI','CE','P') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `doc_num` varchar(50) DEFAULT NULL,
+  `sexo` enum('M','F') DEFAULT NULL,
+  `fecha_registro` date DEFAULT NULL
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movimientos`
+--
+
+CREATE TABLE `movimientos` (
+  `id` int NOT NULL,
+  `id_alquiler` int DEFAULT NULL,
+  `id_recepcionista` int DEFAULT NULL,
+  `tipo` enum('CHECK-IN','CHECK-OUT','ENTRADA','SALIDA') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `fecha_movimiento` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pagos`
+--
+
+CREATE TABLE `pagos` (
+  `id` int NOT NULL,
+  `tipo_pago` enum('EFECTIVO','YAPE') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `monto` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recepcionistas`
+--
+
+CREATE TABLE `recepcionistas` (
+  `id` int NOT NULL,
+  `nombres` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `dni` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `correo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `telefono` varchar(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `clave` varchar(100) DEFAULT NULL,
+  `imagen` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `id` int NOT NULL,
+  `id_alquiler` int NOT NULL,
+  `check_in` date NOT NULL,
+  `check_out` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alquileres`
+--
+ALTER TABLE `alquileres`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_alquier_huesped` (`id_huesped`),
+  ADD KEY `fk_alquier_recepcionista` (`id_recepcionista`),
+  ADD KEY `fk_alquier_habitacion` (`id_habitacion`),
+  ADD KEY `fk_alquiler_pago` (`id_pago`);
+
+--
+-- Indexes for table `habitaciones`
+--
+ALTER TABLE `habitaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_habitacion_alquiler` (`id_alquiler`);
+
+--
+-- Indexes for table `huespedes`
+--
+ALTER TABLE `huespedes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `doc_num` (`doc_num`);
+
+--
+-- Indexes for table `movimientos`
+--
+ALTER TABLE `movimientos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_movimiento_recepcionista` (`id_recepcionista`),
+  ADD KEY `fk_movimiento_alquiler` (`id_alquiler`);
+
+--
+-- Indexes for table `pagos`
+--
+ALTER TABLE `pagos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `recepcionistas`
+--
+ALTER TABLE `recepcionistas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `dni` (`dni`);
+
+--
+-- Indexes for table `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_reserva_alquiler` (`id_alquiler`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alquileres`
+--
+ALTER TABLE `alquileres`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `huespedes`
+--
+ALTER TABLE `huespedes`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `movimientos`
+--
+ALTER TABLE `movimientos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pagos`
+--
+ALTER TABLE `pagos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `recepcionistas`
+--
+ALTER TABLE `recepcionistas`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `alquileres`
+--
+ALTER TABLE `alquileres`
+  ADD CONSTRAINT `fk_alquier_habitacion` FOREIGN KEY (`id_habitacion`) REFERENCES `habitaciones` (`id`),
+  ADD CONSTRAINT `fk_alquier_huesped` FOREIGN KEY (`id_huesped`) REFERENCES `huespedes` (`id`),
+  ADD CONSTRAINT `fk_alquier_recepcionista` FOREIGN KEY (`id_recepcionista`) REFERENCES `recepcionistas` (`id`),
+  ADD CONSTRAINT `fk_alquiler_pago` FOREIGN KEY (`id_pago`) REFERENCES `pagos` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `habitaciones`
+--
+ALTER TABLE `habitaciones`
+  ADD CONSTRAINT `fk_habitacion_alquiler` FOREIGN KEY (`id_alquiler`) REFERENCES `alquileres` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `movimientos`
+--
+ALTER TABLE `movimientos`
+  ADD CONSTRAINT `fk_movimient_alquiler` FOREIGN KEY (`id_alquiler`) REFERENCES `alquileres` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_movimiento_recepcionista` FOREIGN KEY (`id_recepcionista`) REFERENCES `recepcionistas` (`id`);
+
+--
+-- Constraints for table `reservas`
+--
+ALTER TABLE `reservas`
+  ADD CONSTRAINT `fk_reserva_alquiler` FOREIGN KEY (`id_alquiler`) REFERENCES `alquileres` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
